795af9ef54c67efa2e863c1919f978fa23c6e388
Michael Sarpong-Boateng <info@sarppsthedev.com>
2023-04-08 11:17:40 +0100

    Rebuild using Vite

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import troll from '../assets/troll-face.png'
+
+function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={troll}
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
+
+export default Header
